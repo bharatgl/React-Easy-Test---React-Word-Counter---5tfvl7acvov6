bbfdd4a050519267b4766dbf64c6e7cdc3341aab
Bharat goyal <56772219+bharatgl@users.noreply.github.com>
2022-06-12 22:54:14 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import '../styles/App.css';
+mport React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
+  const [char, setChar] = useState(0);
+  const [word, setWord] = useState(0);
 
+  const count = (e) => {
+    let newWord = word;
+    if (e.terget.value === " ") {
+      setWord(newWord++);
+    }
+  };
 
   return (
     <div id="main">
+      <input type="range" min="16px" max="32px" id="fontSize-input" />
+      <input type="number" id="char-limit-input" min="50" />
+      <textarea
+        onChange={(e) => {
+          setChar(e.target.value.length);
+        }}
+      ></textarea>
+      <div id="word-counter">{word}</div>
+      <div id="char-counter">{char}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
